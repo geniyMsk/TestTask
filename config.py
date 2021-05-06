6656f2b263b6ef3a8ffc8d0ab0a776b6a27c4051
geniyMsk <83307950+geniyMsk@users.noreply.github.com>
2021-05-06 15:00:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,3 @@ BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа s
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
 provider_token = env.str("PROVIDER_TOKEN")
-
-host = env.str("PGHOST")
-PG_USER = env.str("PG_USER")
-PG_PASS = env.str("PG_PASS")
